2ebaddfd208d85847072e5bf3e3ee0425e7df610
DaFonz <brandonjfontes@gmail.com>
2021-04-06 12:12:09 -0600

    fixed links

---
+++ client/src/services/EmployeeService.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const EMPLOYEE_API_BASE_URL = "https://radiant-retreat-06528.herokuapp.com/api/register";
+
+class EmployeeService {
+  getEmployees() {
+    return axios.get(EMPLOYEE_API_BASE_URL);
+  }
+
+  createEmployee(employee) {
+    return axios.post(EMPLOYEE_API_BASE_URL, employee);
+  }
+
+  getEmployeeById(employeeId) {
+    return axios.get(EMPLOYEE_API_BASE_URL + "/" + employeeId);
+  }
+
+  updateEmployee(employee, employeeId) {
+    return axios.put(EMPLOYEE_API_BASE_URL + "/" + employeeId, employee);
+  }
+
+  deleteEmployee(employeeId) {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    };
+    return axios.delete(EMPLOYEE_API_BASE_URL + "/" + employeeId, config);
+  }
+}
+
+export default new EmployeeService();
--- client/src/services/SalaryService.js
+++ client/src/services/SalaryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/salary";
+const EMPLOYEE_API_BASE_URL = "https://radiant-retreat-06528.herokuapp.com/api/salary";
 
 class SalaryService {
 
