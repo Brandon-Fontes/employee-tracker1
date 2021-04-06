9428c0a90c90130d08be6ceb690693cf6da63a43
DaFonz <brandonjfontes@gmail.com>
2021-04-06 12:42:15 -0600

    working on fixes

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const path = require('path');
-const cors = require("cors");
 const routes = require("./routes");
 const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-
-
-
-
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-    preflightContinue: true,
-  })
-);
-app.use(routes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,8 +13,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-const PORT = process.env.PORT || 8080;
+
+app.use(routes);
+
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/registerdusers", {
